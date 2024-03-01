2efadea505e49d174c4b16738e64859803c102d5
mahdi khosh khabar <143448758+mahdi-khoshkhabar@users.noreply.github.com>
2024-03-02 01:06:56 +0330

    localization changed from ltr to rtl

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:currency/component/my_colors.dart';
 import 'package:currency/component/text_style.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,6 +13,15 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'Localizations Sample App',
+      localizationsDelegates: const [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: const [
+        Locale('fa'), // Farsi
+      ],
       home: Scaffold(
         backgroundColor: SolidColors.backgroundColor,
         appBar: AppBar(
