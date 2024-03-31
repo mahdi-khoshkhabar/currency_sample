9b3cd6c45875fa20b7931f53079392b66dfe1eb8
mahdi khosh khabar <143448758+mahdi-khoshkhabar@users.noreply.github.com>
2024-03-31 15:42:50 +0330

    ConstrainedBox widget added

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      title: 'Localizations Sample App',
+      title: 'Localizations my App',
       localizationsDelegates: [
         GlobalMaterialLocalizations.delegate,
         GlobalWidgetsLocalizations.delegate,
